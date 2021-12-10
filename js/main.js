bd94a32193d30969ccdd8a3de96ddbea07b78be9
jason-moonKor <jaesangv@naver.com>
2021-12-10 23:46:48 +0900

    update 0.1v

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// 전체카테고리를 클릭했을때 나오는 토글버튼기능
+const toggleBtn = document.querySelector(".category");
+const menu = document.querySelector(".btn_menu");
+
+toggleBtn.addEventListener("click", () => {
+	menu.classList.toggle("active");
+});
+
+// 맨위로 가기 버튼기능
+const backToTop = () => {
+	// Scroll | button show/hide
+	window.addEventListener("scroll", () => {
+		if (document.querySelector("html").scrollTop > 100) {
+			document.getElementById("go-top").style.display = "block";
+		} else {
+			document.getElementById("go-top").style.display = "none";
+		}
+	});
+	// back to top
+	document.getElementById("go-top").addEventListener("click", () => {
+		window.scrollTo({
+			top: 0,
+			left: 0,
+			behavior: "smooth"
+		});
+	});
+};
+backToTop();
+
+// 제이쿼리를 사용해서 페이지 첫화면에 로딩바 노출후 2초후에 fadeOut
+$(function () {
+	$(".open_page, .loader").delay(2000).fadeOut();
+});
